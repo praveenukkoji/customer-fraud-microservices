4b637c0a4ccbf4467bd541da69eaddc09923dc7b
praveenukkoji <praveenukkoji@gmail.com>
2022-09-25 17:00:25 +0530

    register customer post request added, docker compose file added

---
+++ customer/src/main/java/com/praveenukkoji/customer/CustomerController.java
@@ -0,0 +1,26 @@
+package com.praveenukkoji.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "api/v1/customers")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @PostMapping()
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+        log.info("New customer registration {}", customerRegistrationRequest);
+        this.customerService.registerCustomer(customerRegistrationRequest);
+    }
+
+}
