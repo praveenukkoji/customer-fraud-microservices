d13f745d82b9b52496b930378d869f9dfa563371
praveenukkoji <praveenukkoji@gmail.com>
2022-10-02 12:34:50 +0530

    eureka server setup completed

--- eureka-server/src/main/java/com/praveenukkoji/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/praveenukkoji/eurekaserver/EurekaServerApplication.java
@@ -1,14 +1,13 @@
-package com.praveenukkoji.customer;
+package com.praveenukkoji.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class CustomerApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CustomerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
